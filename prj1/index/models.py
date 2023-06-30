cb37d8988853e415705c3c75923ec4631580756d
chumakov <vgff@gmail.com>
2023-06-30 12:10:19 +0900

    kkk

--- prj1/index/models.py
+++ prj1/index/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class users(models.Model):
+    user_login = models.CharField(max_length=30)
+    user_password = models.CharField(max_length=30)
+
+class users(models.Model):
+    user_login = models.CharField(max_length=30)
+    user_password = models.CharField(max_length=30)
+
